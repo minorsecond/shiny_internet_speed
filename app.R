da4baea716478fa5219f83d1bd7731cc0ecd8721
Ross Wardrup <rwardrup@Rosss-iMac.local>
2017-11-28 05:48:51 -0600

    Fixes / features

--- app.R
+++ app.R
@@ -1,6 +1,6 @@
 # My subscription is for 100Mbps down, 10  Mbps up
 #
-
+rm(list = ls())
 library(shiny)
 library(shinydashboard)
 library(RPostgreSQL)
@@ -8,7 +8,8 @@ library(ggplot2)
 library(gridExtra)
 #library(keyringr)
 
-source("global.R")
+rm(plots)
+#source("global.R")
 
 mydashboardHeader <- function(..., title = NULL, disable = FALSE,title.navbar=NULL, .list = NULL) {
   items <- c(list(...), .list)
@@ -41,6 +42,15 @@ ui <- dashboardPage(
   mydashboardHeader(title = "Charter Internet Speed Tests"),
   dashboardSidebar(width = 0),
   dashboardBody(
+    tags$head(tags$style(
+      type="text/css",
+      "#binned_down img, #binned_up img {
+    width: 80%;
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    }")),
+    includeCSS("www/custom.css"),
     tabsetPanel(
       #tags$head(tags$style("#all_down_up_speeds{height:100vh !important;}")),
       tabPanel(title = "Speeds Over Time",
@@ -51,13 +61,20 @@ ui <- dashboardPage(
                  plotOutput("date_upload")
                  )
                ),
+      tabPanel(title = "Speeds Binned by Hour of Day",
+               tabsetPanel(
+                 tabPanel(title = "Download",
+                          plotOutput("binned_down", width = "100%", height = "100%")),
+                 tabPanel(title = "Upload",
+                          plotOutput("binned_up", width = "100%", height = "100%")))),
       tabPanel(title = "Overall Download / Upload Speeds",
                plotOutput("all_down_up_speeds")),
       tabPanel(title = "About",
                br(),
                p("After calling Charter/Spectrum numberous times about slow or 
                  nonexistant internet service, I wanted to emperically show
-                 what was going on. Using R and Shiny, I created this webapp
+                 what was going on. We subscribe to Spectrum's 100 Mb per second 
+                 down / 10 Mb per seocnd up service. Using R and Shiny, I created this webapp
                  to display my internet speed over time."),
                br(),
                br(),
@@ -75,12 +92,25 @@ ui <- dashboardPage(
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
+  source("global.R")
   output$date_download <- renderPlot(
     plots$download_speed
   )
   
   output$date_upload <- renderPlot(
     plots$upload_speed
+  )
+  
+  output$binned_down <- renderPlot(
+    plots$time.of.day.down.speed,
+    height = 600,
+    width = 800
+  )
+  
+  output$binned_up <- renderPlot(
+    plots$time.of.day.up.speed,
+    height = 600,
+    width = 800
   )
 
   output$all_down_up_speeds <- renderPlot(
--- global.R
+++ global.R
@@ -1,18 +1,26 @@
 library(RPostgreSQL)
 library(mgcv)
 library(scales)
-source("pub_graphs.R")
+library(ggplot2)
+library(reshape2)
 
-pw <- {
-  "Rward0232"
-}
-drv <- dbDriver("PostgreSQL")
+source("pub_graphs.R")
 
 data_download_time <- strptime(format(file.info("testResults.Rds")$ctime), format = "%Y-%m-%d %H:%M:%S")
 file_age <- Sys.time() - data_download_time
-query.start.time <- Sys.time()
-if (file_age > 930 | !file.exists("testResults.Rds")) {
+
+print(paste("file age: ", file_age, sep = ""))
+if (file_age > 900 | !file.exists("testResults.Rds")) {
+  rm(list = ls())
+  
+  source("pub_graphs.R")
+  query.start.time <- Sys.time()
   plots = list()
+  means = list()
+  pw <- {
+    "Rward0232"
+  }
+  drv <- dbDriver("PostgreSQL")
   cat(file=stderr(), "testResults.Rds has expired. Downloading fresh data")
  # print("testResults.Rds has expired. Downloading fresh data")
   print(paste("Connecting to PG DB at ", 
@@ -44,22 +52,47 @@ if (file_age > 930 | !file.exists("testResults.Rds")) {
   
   test_results$Timestamp <- as.POSIXct(strptime(test_results$Timestamp, 
                                                "%Y-%m-%dT%H:%M", 
-                                               tz="UTC"), tz = "UTC")
+                                               tz="UTC"), tz = "America/Chicago") - (6*3600)
   
   test_results$Download_mb <- test_results$Download * 0.000000954
   test_results$Upload_mb <- test_results$Upload * 0.000000954
   test_results$Distance_mi <- test_results$Distance * 0.621371
+  
+  test_results$hour_of_day <- as.integer(format(test_results$Timestamp, "%H"))
+  means$mean0.4. <- with(test_results, mean(Download_mb[hour_of_day > 0 & hour_of_day < 4]))
+  means$mean4.8. <- with(test_results, mean(Download_mb[hour_of_day > 4 & hour_of_day < 8]))
+  means$mean8.12. <- with(test_results, mean(Download_mb[hour_of_day > 8 & hour_of_day < 12]))
+  means$mean12.16. <- with(test_results, mean(Download_mb[hour_of_day > 12 & hour_of_day < 16]))
+  means$mean16.20. <- with(test_results, mean(Download_mb[hour_of_day > 16 & hour_of_day < 20]))
+  means$mean20.24. <- with(test_results, mean(Download_mb[hour_of_day > 20 & hour_of_day < 24]))
+  means <- melt(means)
+  colnames(means) <- c("speed", "time.window")
+  means$measurement <- "down"
+  
+  means_ = list()
+  means_$mean0.4. <- with(test_results, mean(Upload_mb[hour_of_day > 0 & hour_of_day < 4]))
+  means_$mean4.8. <- with(test_results, mean(Upload_mb[hour_of_day > 4 & hour_of_day < 8]))
+  means_$mean8.12. <- with(test_results, mean(Upload_mb[hour_of_day > 8 & hour_of_day < 12]))
+  means_$mean12.16. <- with(test_results, mean(Upload_mb[hour_of_day > 12 & hour_of_day < 16]))
+  means_$mean16.20. <- with(test_results, mean(Upload_mb[hour_of_day > 16 & hour_of_day < 20]))
+  means_$mean20.24. <- with(test_results, mean(Upload_mb[hour_of_day > 20 & hour_of_day < 24]))
+  means_ <- melt(means_)
+  colnames(means_) <- c("speed", "time.window")
+  means_$measurement <- "up"
+  
+  means <- rbind(means, means_)  # Combine the two dataframes
   
   plots$download_speed <- ggplot(test_results, 
                                  aes(x = Timestamp,
                                      y = Download_mb)) +
     geom_hline(yintercept = 100, colour = 'red', aes(linetype = "Subscription Speed")) +
     #scale_linetype_manual(values = c("Subscription Speed"), guide = guide_legend(override.aes = list(color = c("red")))) +
-    annotate("text", x = sort(test_results$Timestamp)[2] + 20, y = 100, vjust = -.5, label = "Subscribed Download Speed") +
+    annotate("text", x = median(test_results$Timestamp), y = 100, vjust = -.5, label = "Subscribed Download Speed") +
     geom_point(aes(color = Sponsor)) +
     #geom_smooth(method = 'lm', formula = y ~ poly(x, 2), size = 1) +
-    stat_smooth(method = "gam", formula = y ~ s(x, k = 6), alpha = .15) +
-    #scale_y_sqrt() +
+    stat_smooth(method = "gam", formula = y ~ s(x, k = 6), size = .75, level = .99) +
+    #scale_y_log10() +
+    scale_y_sqrt() +
     scale_x_datetime(labels = date_format("%d-%m-%Y", 
                                           tz = "America/Chicago")) +
     theme_Publication() +
@@ -73,15 +106,15 @@ if (file_age > 930 | !file.exists("testResults.Rds")) {
                                aes(x = Timestamp,
                                    y = Upload_mb)) +
     geom_hline(yintercept = 10, color = "red", aes(linetype = "Subscription Speed")) +
-    annotate("text", x = sort(test_results$Timestamp)[2], y = 10, vjust = -.5, label = "Subscribed Upload Speed") +
+    annotate("text", x = median(test_results$Timestamp), y = 10, vjust = -.5, label = "Subscribed Upload Speed") +
     geom_point(aes(color = Sponsor)) +
     #geom_smooth(method = 'lm', formula = y ~ poly(x, 2), size = 1) +
-    stat_smooth(method = "gam", formula = y ~ s(x, k = 6), alpha = .15) +
+    stat_smooth(method = "gam", formula = y ~ s(x, k = 6), size = .75, level = .99) +
+    #scale_y_log10() +
     scale_y_sqrt() +
     scale_x_datetime(labels = date_format("%d-%m-%Y", 
                                           tz = "America/Chicago")) +
     theme_Publication() +
-    scale_colour_Publication() +
     labs(title = "Charter Internet Upload Speed",
          x = "Local Time (CST)",
          y = "Upload Speed in Megabits per Second",
@@ -109,16 +142,119 @@ if (file_age > 930 | !file.exists("testResults.Rds")) {
     labs(title = "Charter Internet Upload Speed - All Dates",
          y = "Upload Speed in Megabits per Second")
   
+  plots$time.of.day.down.speed <- ggplot(test_results, 
+                                    aes(x = hour_of_day, 
+                                        y = Download_mb)) + 
+    stat_binhex(bins = 10) + 
+    theme_Publication() +
+    scale_fill_gradient(low = "forestgreen", 
+                        high = "firebrick") +
+    labs(title = "Charter Internet Download Speed - Time of Day",
+         x = "Hour of Day",
+         y = "Download Speed in Megabits per Second",
+         fill = "Count per Bin") +
+    theme(legend.key.height = unit(1, "line"))
+  
+  plots$time.of.day.up.speed <- ggplot(test_results, 
+                                    aes(x = hour_of_day, 
+                                        y = Upload_mb)) + 
+    stat_binhex(bins = 10) + 
+    theme_Publication() +
+    scale_fill_gradient(low = "forestgreen", 
+                        high = "firebrick") +
+    labs(title = "Charter Internet Upload Speed - Time of Day",
+         x = "Hour of Day",
+         y = "Upload Speed in Megabits per Second",
+         fill = "Count per Bin") +
+    theme(legend.key.height = unit(1, "line"))
+  
+  
   saveRDS(test_results, file = "testResults.Rds")
   saveRDS(plots, "plots.Rds")
   #print("Saved new testResults.Rds file")
-} else if (!exists("plots")) {
+} else if (!exists("plots") & exists("plots.Rds")) {
   cat(file=stderr(), "testResults.Rds isn't expired and plots aren't loaded... loading plots from disk.")
   #print("testResults.Rds isn't expired and plots aren't loaded... loading plots from disk.")
   plots <- readRDS("plots.Rds")
   #print("Finished loading Plots")
+} else if (!exists("plots") & !exists("plots.Rds") & file.exists("testResults.Rds")) {
+  test_results <- readRDS("testResults.Rds")
+  plots = list()
+  
+  plots$download_speed <- ggplot(test_results, 
+                                 aes(x = Timestamp,
+                                     y = Download_mb)) +
+    geom_hline(yintercept = 100, colour = 'red', aes(linetype = "Subscription Speed")) +
+    #scale_linetype_manual(values = c("Subscription Speed"), guide = guide_legend(override.aes = list(color = c("red")))) +
+    annotate("text", x = sort(test_results$Timestamp)[2] + 20, y = 100, vjust = -.5, label = "Subscribed Download Speed") +
+    geom_point(aes(color = Sponsor)) +
+    #geom_smooth(method = 'lm', formula = y ~ poly(x, 2), size = 1) +
+    stat_smooth(method = "gam", formula = y ~ s(x, k = 6), alpha = .15) +
+    #scale_y_sqrt() +
+    scale_x_datetime(labels = date_format("%d-%m-%Y", 
+                                          tz = "America/Chicago")) +
+    theme_Publication() +
+    scale_colour_Publication() +
+    labs(title = "Charter Internet Download Speed",
+         x = "Local Time (CST)",
+         y = "Download Speed in Megabits per Second",
+         col = "Speed Test Server Sponsor")
+  
+  plots$upload_speed <- ggplot(test_results, show.legend = F,
+                               aes(x = Timestamp,
+                                   y = Upload_mb)) +
+    geom_hline(yintercept = 10, color = "red", aes(linetype = "Subscription Speed"), show.legend = F) +
+    annotate("text", x = sort(test_results$Timestamp)[2], y = 10, vjust = -.5, label = "Subscribed Upload Speed") +
+    geom_point(aes(color = Sponsor), show.legend = F) +
+    #geom_smooth(method = 'lm', formula = y ~ poly(x, 2), size = 1) +
+    stat_smooth(method = "gam", formula = y ~ s(x, k = 6), alpha = .15, show.legend = F) +
+    scale_y_sqrt() +
+    scale_x_datetime(labels = date_format("%d-%m-%Y", 
+                                          tz = "America/Chicago")) +
+    theme_Publication() + 
+    labs(title = "Charter Internet Upload Speed",
+         x = "Local Time (CST)",
+         y = "Upload Speed in Megabits per Second") +
+    theme(legend.position = "none")
+  
+  plots$overall_down_speeds <- ggplot(test_results,
+                                      aes(y = Download_mb, x = NA)) +
+    geom_boxplot() +
+    theme_Publication() +
+    scale_colour_Publication() +
+    theme(axis.title.x = element_blank(),
+          axis.text.x = element_blank(),
+          axis.ticks.x = element_blank()) +
+    labs(title = "Charter Internet Download Speed - All Dates",
+         y = "Download Speed in Megabits per Second")
+  
+  plots$overall_up_speeds <- ggplot(test_results,
+                                    aes(y = Upload_mb, x = NA)) +
+    geom_boxplot() +
+    theme_Publication() +
+    scale_colour_Publication() +
+    theme(axis.title.x = element_blank(),
+          axis.text.x = element_blank(),
+          axis.ticks.x = element_blank()) +
+    labs(title = "Charter Internet Upload Speed - All Dates",
+         y = "Upload Speed in Megabits per Second")
+  
+  plots$time.of.day.speed <- ggplot(test_results, 
+                                    aes(x = hour_of_day, 
+                                        y = Download_mb)) + 
+    stat_binhex(bins = 10) + 
+    theme_Publication() +
+    scale_fill_gradient(low = "forestgreen", 
+                        high = "firebrick") +
+    labs(title = "Charter Internet Download Speed - Time of Day",
+         x = "Hour of Day",
+         y = "Download Speed in Megabits per Second",
+         fill = "Count per Bin") +
+    theme(legend.key.height = unit(1, "line"))
+  
 } else {
+  rm(plots)
   cat(file=stderr(), "Plots already loaded - displaying them now.")
-  #print("Plots already loaded - displaying them now.")
+  plots <- readRDS("plots.Rds")
 }
   
