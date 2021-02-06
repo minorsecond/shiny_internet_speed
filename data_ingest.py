e14f9876fc8419573ffad632cb586bafc813b521
Ross Wardrup <minorsecond@gmail.com>
2021-02-06 13:04:32 +0000

    Get relative path

--- data_ingest.py
+++ data_ingest.py
@@ -5,9 +5,12 @@ import os
 print("connecting to 192.168.3.65")
 conn = psycopg2.connect("host=192.168.3.65 dbname=internetStatus user=rwardrup password=Rward0232")
 cur = conn.cursor()
+current_path = os.path.dirname(os.path.realpath(__file__))
+
+print(f"Working path: {current_path}")
 
 # Read CSV
-with open("./data/speedtest_data.csv", 'r') as f:
+with open(f"{current_path}/data/speedtest_data.csv", 'r') as f:
   reader = csv.reader(f)
   for row in reader:
     print("Uploading {0}".format(row))
